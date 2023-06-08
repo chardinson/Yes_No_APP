194b99d8c73aae6422d64a0ce56d62db141911dd
chardinson <chardinson05082002@gmail.com>
2023-06-08 00:16:32 -0400

    Provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> message = [
+    Message(text: 'Saludos', fromWho: FromWho.me),
+    Message(text: 'Tengo un problema con mi cuenta', fromWho: FromWho.me)
+  ];
+
+  Future<void> sendMessage(String text) async {}
+}
