7fa4971ef1b133a67d50c46bc19637eaf1deabeb
chardinson <chardinson05082002@gmail.com>
2023-06-09 02:15:50 -0400

    Mostrar los mensajes del provider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messagesList = [
     Message(text: 'Saludos', fromWho: FromWho.me),
     Message(text: 'Tengo un problema con mi cuenta', fromWho: FromWho.me)
   ];
