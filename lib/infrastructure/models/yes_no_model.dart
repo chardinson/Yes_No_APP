d8ca7fb4f52d1b3d881368c62a0b422d750c674e
chardinson <chardinson05082002@gmail.com>
2023-06-13 10:46:33 -0400

    QuickType.io al rescate

---
+++ lib/infrastructure/models/yes_no_model.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:yes_no_app/domain/entities/message.dart';
+
+YesNoModel yesNoModelFromJson(String str) =>
+    YesNoModel.fromJsonMap(json.decode(str));
+
+String yesNoModelToJson(YesNoModel data) => json.encode(data.toJson());
+
+class YesNoModel {
+  final String answer;
+  final bool forced;
+  final String image;
+
+  YesNoModel({
+    required this.answer,
+    required this.forced,
+    required this.image,
+  });
+
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEnity() =>
+      Message(text: answer, fromWho: FromWho.her, imageUrl: image);
+}
