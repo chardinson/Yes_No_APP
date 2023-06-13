2e8d1fade5b0f359d9af30a0a4dcc4311b99f555
chardinson <chardinson05082002@gmail.com>
2023-06-13 10:13:00 -0400

    Respuesta de YesNo

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
   final ScrollController chatScrollController = ScrollController();
   List<Message> messagesList = [];
 
@@ -9,8 +11,15 @@ class ChatProvider extends ChangeNotifier {
     if (text.isEmpty) return;
     final sendMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(sendMessage);
+    if (text.endsWith('?')) {
+      herReplay();
+    }
     notifyListeners();
     moveScrollToBottom();
+  }
+
+  Future<void> herReplay() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
   }
 
   Future<void> moveScrollToBottom() async {
